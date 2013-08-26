b51eab820fe0e27559015d36850dff274d219502
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-26 11:55:09 -0500

    added models and bcrypt gem

---
+++ app/models/comment.rb
@@ -0,0 +1,8 @@
+class Comment < ActiveRecord::Base
+  attr_accessible :text
+
+  belongs_to :post
+  belongs_to :user
+
+  validates :text, presence: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password_digest
+
+  has_many :posts
+  has_many :comments
+
+  validates :email, :name, presence: true
+  validates :email, uniqueness: true
+
+  has_secure_password
 end

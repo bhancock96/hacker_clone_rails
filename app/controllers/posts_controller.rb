71af7a87ceeb9a629091fb65fc14498a52c300ee
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-26 18:10:47 -0500

    added post creation form, current user helper, and post actions to controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,22 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    # @current_user.post.create(params[:post])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
